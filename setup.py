171725a89837e2f745e78f0783fe3638a0cfbf1c
Alex-Roland <rolanda@purdue.edu>
2021-10-22 13:28:40 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 from act.act import version_info
 
 setup(
-    name='ACL Check Tool',
+    name='act',
     version=version_info().VERSION,
     description='Search ACLs for a matching IP',
     author='Alex Roland',
     author_email='alex.roland@peacefulnetworks.com',
-    url='https://github.com/Alex-Roland/acl-check-tool',
+    url='https://github.com/Alex-Roland/act',
     license='MIT',
     entry_points={
         'console_scripts': [
