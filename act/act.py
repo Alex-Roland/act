a9fe10b768bf213127a6548ce97bb0e599ad61ce
Alex-Roland <47874780+Alex-Roland@users.noreply.github.com>
2022-07-27 07:56:49 -0500

    Update act.py

--- act/act.py
+++ act/act.py
@@ -2,7 +2,7 @@
 
 # MIT License
 #
-# Copyright (c) 2021 Alex Roland
+# Copyright (c) 2022 Alex Roland
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,6 +25,7 @@
 import argparse
 import requests
 import getpass
+import ipaddress
 import re
 import os
 from threading import Thread, currentThread, Lock
@@ -35,46 +36,53 @@ from urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(category = InsecureRequestWarning) # This will disable certificate warning output when running the script
 
-parser = argparse.ArgumentParser(description = 'Checks ACLs for an IP match')
-parser.add_argument('-i', help = 'ip to check (can be comma separated)')
-parser.add_argument('-s', help = 'XMC hostname / IP (default: xmc)', default = 'xmc')
-parser.add_argument('-u', help = 'username for SSH authentication (default: admin)', default = 'admin')
-parser.add_argument('-t', help = 'number of threads to run (default: 20)', type=int, default = 20)
-parser.add_argument('-w', help = 'filename to write report to (default: [time]-[ip]-report.log)', default = 'report.log')
-parser.add_argument('-n', help = 'do not validate HTTPS certificates (default: false)', default = False, action = 'store_true')
-parser.add_argument('-V', help = 'display version information', default = False, action = 'store_true')
+parser = argparse.ArgumentParser(description='Checks ACLs for an IP match')
+parser.add_argument('-i', metavar='ip', help='ip to check (can be comma separated)')
+parser.add_argument('-s', metavar='nms', help='XMC hostname / IP (default: xmc)', default='xmc')
+parser.add_argument('-u', metavar='user', help='username for SSH authentication (default: admin)', default='admin')
+parser.add_argument('-t', metavar='threads', help='number of threads to run (default: 20)', type=int, default=20)
+parser.add_argument('-w', metavar='file', help='filename to write report to (default: [time]-[ip]-report.log)', default='report.log')
+parser.add_argument('-a', help='write all acl output to a file', default=False, action='store_true')
+parser.add_argument('-n', help='do not validate HTTPS certificates (default: false)', default=False, action='store_true')
+parser.add_argument('-V', help='display version information', default=False, action='store_true')
 args = parser.parse_args()
 
 def version_info():
     NAME = 'ACL Check Tool'
-    VERSION = '2.3.5'
-    DATE = '10/22/2021'
+    VERSION = '2.5.0'
+    DATE = '05/02/2022'
 
     print('|' + '-'*30 + '|')
     print('|' + ' '*8 + NAME + ' '*8 + '|')
     print('|' + '-'*30 + '|')
     print('|' + ' '*4 + VERSION + ' '*5 + '|' + ' '*3 + DATE + ' '*2 + '|')
     print('|' + '-'*30 + '|')
+    exit(0)
 
 if args.V == True: # Script version information
     version_info()
-    exit(0)
 
-if args.i is None: # Checks if user provided an IP to check against
-    print('Please provide at least one IP to check')
-    exit(0)
-
-ips = args.i.split(',')
 current_date = datetime.now().strftime('%Y-%m-%d')
-failed = False # Used to let the user know if there is anything in the failure log
+current_time = datetime.now().strftime('%Y_%d_%m_%H%M%S')
 
-NUM_THREADS = args.t # Used to set the maximum amount of parallel connections
 PRINT_LOCK = Lock() # This will prevent the output from being sent out of order
 
 # Script colors
 warningColor = "\033[91m"
 resetColor = "\033[0m"
 os.system('') # Needed to trigger color detection
+
+def acl_folder():
+    """
+    Create an ACL output folder if one does not exist.
+    """
+    if os.path.isdir('acls'):
+        return
+    else:
+        try:
+            os.mkdir('acls')
+        except BaseException as e:
+            log_failed(e)
 
 def log_folder():
     """
@@ -87,8 +95,6 @@ def log_folder():
             os.mkdir('logs')
         except BaseException as e:
             log_failed(e)
-            global failed
-            failed = True
 
 def daily_runtime_folder():
     """
@@ -101,8 +107,6 @@ def daily_runtime_folder():
             os.mkdir('logs/' + current_date)
         except BaseException as e:
             log_failed(e)
-            global failed
-            failed = True
 
 def log_file(file): # Function to output to the screen and log the output to a file
     """
@@ -144,7 +148,7 @@ def discover_vendor(nickName, nosIdName):
     convention. For example, for il-chi-x690-c1 you would check for 'x' in the second to last index after splitting on the '-'.
     We grab the NOS so commands are run correctly based on the NOS syntax.
     """
-    if '-' in nickName and 'pnw.edu' not in nickName and 'purdue.edu' not in nickName:
+    if '-' in nickName:
         letter = nickName.split('-')[-2][0]
     else:
         logger(nickName + ' does not conform to naming conventions')
@@ -187,7 +191,7 @@ def run_threads(total_devices, mt_function, dq, dd, ip_list): # dq is the device
     """
     Builds and kicks off the hyper-threading. Number of threads is defaulted to 20, but can be overridden with the -t argument.
     """
-    total_threads = min(NUM_THREADS, total_devices) # If the amount of loaded devices is less than the max thread count, then limit the amount of threads to device count
+    total_threads = min(args.t, total_devices) # If the amount of loaded devices is less than the max thread count, then limit the amount of threads to device count
     for i in range(total_threads):
         thread_name = f'Thread-{i}'
         worker = Thread(name=thread_name, target=mt_function, args=(dq, dd, ip_list))
@@ -201,7 +205,6 @@ def send_commands(dq, dd, ip_list):
     while True:
         thread_name = currentThread().getName()
         ipcounter = 0
-        global failed
 
         if dq.empty(): # Check if device queue is empty
             logger(f'{thread_name}: Closing since there are no jobs left in the queue.')
@@ -222,32 +225,46 @@ def send_commands(dq, dd, ip_list):
                     policy = re.findall(r"[a-zA-Z0-9_\\\-\.\(\):]+\.pol", nc.send_command('ls'))
                     logger(f'{thread_name} {ip} output:\n{policy}')
                     if policy != []:
-                        for i in range(len(policy)):
-                            policyfile = policy[i].strip('.pol')
-                            for addr in ip_list:
-                                ipcheck = nc.send_command_timing(f'show policy {policyfile} | i {ip_list[ipcounter]}')
-                                if re.search(rf'\b{ip_list[ipcounter]}\b', ipcheck) and not "Unrecognized" in ipcheck:
-                                    output = policyfile + " has line " + ipcheck
-                                    logger(f'{thread_name} {ip} output:\n' + output)
-                                    out_sum(f'{ip}({hostname}) --> {output}')
-                                ipcounter += 1
-                            ipcounter = 0
+                        if args.a:
+                            output = nc.send_command('show policy detail')
+                            with open(f'acls/all_acls_{current_time}.txt', 'a') as f:
+                                print(f'{"-"*40}\n{hostname}\n{"-"*40}\n{output}', file=f)
+                        else:
+                            for i in range(len(policy)):
+                                policyfile = policy[i].strip('.pol')
+                                for addr in ip_list:
+                                    ipcheck = nc.send_command_timing(f'show policy {policyfile} | i {addr}')
+                                    if re.search(rf'\b{addr}\b', ipcheck) and not "Unrecognized" in ipcheck:
+                                        output = policyfile + " has line " + ipcheck
+                                        logger(f'{thread_name} {ip} output:\n' + output)
+                                        out_sum(f'{ip}({hostname}) --> {output}')
                     else:
                         logger('No policy found')
                 elif nc_params['device_type'] == 'enterasys':
                     output = nc.send_command('show access-lists')
-                    check_ip(output, ip_list, ip, hostname)
+                    if args.a:
+                        with open(f'acls/all_acls_{current_time}.txt', 'a') as f:
+                            print(f'{"-"*40}\n{hostname}\n{"-"*40}\n{output}', file=f)
+                    else:
+                        check_ip(output, ip_list, ip, hostname)
                 elif nc_params['device_type'] == 'cisco_ios':
                     output = nc.send_command('show access-lists')
-                    check_ip(output, ip_list, ip, hostname)
+                    if args.a:
+                        with open(f'acls/all_acls_{current_time}.txt', 'a') as f:
+                            print(f'{"-"*40}\n{hostname}\n{"-"*40}\n{output}', file=f)
+                    else:
+                        check_ip(output, ip_list, ip, hostname)
                 elif nc_params['device_type'] == 'cisco_nxos':
                     output = nc.send_command('show access-lists')
-                    check_ip(output, ip_list, ip, hostname)
+                    if args.a:
+                        with open(f'acls/all_acls_{current_time}.txt', 'a') as f:
+                            print(f'{"-"*40}\n{hostname}\n{"-"*40}\n{output}', file=f)
+                    else:
+                        check_ip(output, ip_list, ip, hostname)
                 else:
                     raise
             except BaseException as e:
                 log_failed(f'{thread_name} {ip}: {e}')
-                failed = True
                 nc.disconnect() # Gracefully closes the SSH connection
                 dq.task_done() # Indicate that a formerly enqueued task is complete
                 return
@@ -256,13 +273,12 @@ def send_commands(dq, dd, ip_list):
             dq.task_done() # Indicate that a formerly enqueued task is complete
         except BaseException as e:
             log_failed(f'{thread_name} {ip}:\n {e}')
-            failed = True
             dq.task_done() # Indicate that a formerly enqueued task is complete
 
 if args.w == 'report.log': # Creates a unique report name
-    args.w = f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}-{ips[0]}-report.log' # Standard log file to log all events
-    failedfile = f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}-{ips[0]}-failed-report.log' # If there are any issues, output to a failed file
-    sumfile = f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}-{ips[0]}-summary-report.log' # Output the success to separate file
+    args.w = f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}-report.log' # Standard log file to log all events
+    failedfile = f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}-failed-report.log' # If there are any issues, output to a failed file
+    sumfile = f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}-summary-report.log' # Output the success to separate file
 else:
     failedfile = f'{args.w}-failed-report.log'
     sumfile = f'{args.w}-summary-report.log'
@@ -272,9 +288,25 @@ log_failed = log_file(failedfile) # Set log file if there is a failed device
 out_sum = log_sum(sumfile) # Set log file for summary output
 
 def main():
+    if args.i is None and args.a == False: # Checks if user provided an IP to check against
+        print('Please provide at least one IP to check')
+        exit(0)
+
+    if args.i is not None and args.a == False:
+        try:
+            ipaddress.ip_address(args.i)
+            ips = args.i.split(',')
+            ip_list = [addr.replace('.', '\.') for addr in ips] # Converts each IP in the list to escape the dot between the octects
+        except ValueError:
+            ip_list = args.i.split(',')
+    
+    if args.a == True:
+        ip_list = None
+
     password = getpass.getpass(f'{args.u} password:')
     
     log_folder()
+    acl_folder()
     daily_runtime_folder()
 
     api_url = 'https://' + args.s + ':8443/nbi/graphql' # URL of the XMC server, pulled from -s flag in argparse
@@ -291,9 +323,6 @@ def main():
         'password': '',
         'conn_timeout': 10,
     }
-
-    ip_list = [addr.replace('.', '\.') for addr in ips] # Converts each IP in the list to escape the dot between the octects
-    global failed
 
     logger('Connecting...')
     out_sum('Connecting...')
@@ -339,10 +368,9 @@ def main():
     except BaseException as e:
         print(f'Failed to fetch data, check connection to "{args.s}"')
         log_failed('Failed to fetch data:')
-        log_failed(e)
-        failed = True
+        log_failed(e)    
     
-    if failed == True:
+    if os.path.exists('logs/'+ current_date + '/' + failedfile):
         print(f'{warningColor}There were some issues, check the failed log output for details{resetColor}')
 
 if __name__ == "__main__": # Check if script is called by the user or imported
